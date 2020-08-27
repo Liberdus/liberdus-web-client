3eec4a41d6c535ed64b1987708e9b85b96c337c1
chainergar <garmoniart@gmail.com>
2020-08-27 16:34:27 -0400

    Fix sign out issue

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -179,8 +179,7 @@ export default {
           localStorage.removeItem('lastTx')
         }
       } catch (e) {}
-      this.$router.push('/welcome')
-      window.location.reload(false)
+      window.location.href = '/'
     }
   }
 }
